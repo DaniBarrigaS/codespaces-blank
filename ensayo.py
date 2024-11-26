e156e50d3393caa63d35cf4db875ce6b09b48fe5
DaniBarrigaS <daniela.barriga@genstudents.org>
2024-11-26 15:35:21 +0000

    first commit

---
+++ ensayo.py
@@ -0,0 +1,7 @@
+# es un comentario y esa linea no se sera ejecutada
+
+#lee variable, le asigna un valor y la guarda en memoria
+nombre = input ("ingresa tu nombre: ")
+apellido= input ("ingresa tu apellido: ")
+cuidad= input ("ingresa tu cuidad: ")
+print (f"hola, {nombre} {apellido} bienvenido a nuestra app")
