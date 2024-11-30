fe8c0d89a8fec9cb5b16674fb65aeb98423b2f50
DaniBarrigaS <daniela.barriga@genstudents.org>
2024-11-30 22:51:55 +0000

    avances

--- ejercicios/ensayo4.py
+++ ejercicios/ensayo4.py
@@ -1,6 +1,10 @@
-numero= (int("input(numero"))
-numero1=(int("input(numero1)")
-suma= int(input {numero}+{numero1})
-resta= int(input {numero}-{numero1})
-print= input(f"suma")
-print= input(f"resta")
+numero=int(input("numero"))
+numero1=int(input("numero1"))
+suma= numero+numero1
+resta= numero-numero1
+multiplicacion= numero*numero1
+division= numero/numero1
+print(f"{suma}")
+print(f"{resta}")
+print(f"{multiplicacion}")
+print(f"{division}")
---
+++ ejercicios/ensayo5.py
@@ -0,0 +1,4 @@
+producto=int(input("producto"))
+iva=float(input("1,19"))
+suma=producto*iva
+print=float(f"{iva}")
